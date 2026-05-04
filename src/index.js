4b110035630b5427dd25e8b85c75b275f76fc735
johnwo90-JL <john-age@jobloop.no>
2026-05-04 13:40:32 +0200

    Refactor: Separated `index.js` and server functionality into separate file `server.js`

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
-import express from "express";
-import { rootRouter, minRouter } from "./routes/index.js";
+import { startServer } from "./server.js";
 
 
-// Express.js app
-export const app = express();
-
-
-// Opprette endepunkt "[hostaddr]/"
-app.use("/", rootRouter);
-app.use("/mine-endepunkter", minRouter);
-
-
-// // Start server
-// app.listen(3000, "0.0.0.0", (err) => {
-//     if (err) {
-//         console.error(err);
-//         process.exit(1); //- == 0 => OK / Graceful exit/termination
-//                          //- != 0 => Feil
-//     }
-
-//     console.log(`Server listening on port 3000.`);
-// });
+startServer();
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { rootRouter, minRouter } from "./routes/index.js";
+
+// Express.js app
+export const app = express();
+
+// Opprette endepunkt "[hostaddr]/"
+app.use("/", rootRouter);
+app.use("/mine-endepunkter", minRouter);
+
+export function startServer(port = 3000, host = "0.0.0.0") {
+    app.listen(port, host, (err) => {
+        if (err) {
+            console.error(err);
+            process.exit(1); //- == 0 => OK / Graceful exit/termination
+                             //- != 0 => Feil
+        }
+    
+        console.log(`Server listening on port 3000.`);
+    });
+}
